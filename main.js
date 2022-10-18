257f1c492ef002f6c9d8fd557a45e535df83f0c7
silfredo11 <ibarrasilfredo@gmail.com>
2022-10-18 14:10:18 -0500

    I add progress bar

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const hamburguerMenu = document.querySelector('.header__hamburguer');
 const mobilMnu= document.querySelector('.mobileMenu');
+const progress = document.querySelector('#progress');
 
 hamburguerMenu.addEventListener('click', toggleHamburguerMenu);
 
@@ -12,4 +13,11 @@ function toggleHamburguerMenu(){
     
 }
 
+window.addEventListener('load', () => {
+  requestAnimationFrame(update);
+})
 
+function update() {
+  progress.style.width = `${((window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100)}%`
+  requestAnimationFrame(update);
+}
