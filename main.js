94d5a169062ceb79588b5c5ffb1ec18cdc45732a
silfredo11 <ibarrasilfredo@gmail.com>
2022-12-06 02:07:07 -0500

    I am making the x icon in the hamburguer menu

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const hamburguerMenu = document.querySelector('.header__hamburguer');
 const mobilMnu= document.querySelector('.mobileMenu');
+const iconMnuBar2 = document.querySelector('.header__hamburguerSons2');
+const iconMnuBar1 = document.querySelector('.header__hamburguerSons1');
+const iconMnuBar3 = document.querySelector('.header__hamburguerSons3');
 const progress = document.querySelector('#progress');
 
 hamburguerMenu.addEventListener('click', toggleHamburguerMenu);
+hamburguerMenu.addEventListener('click', toggleCentralBar);
+hamburguerMenu.addEventListener('click', toggleRotateBar);
 
 
 function toggleHamburguerMenu(){
@@ -11,6 +16,15 @@ function toggleHamburguerMenu(){
 
     console.log('como estas mario');
     
+}
+
+function toggleCentralBar(){
+  iconMnuBar2.classList.toggle('son2');
+}
+
+function toggleRotateBar(){
+  iconMnuBar1.classList.toggle('son1');
+  iconMnuBar3.classList.toggle('son3');
 }
 
 window.addEventListener('load', () => {
